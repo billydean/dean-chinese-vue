43f59b97de98853dbbcf5ad547178fdc8e90ccb8
billydean <billy.goehring@gmail.com>
2023-07-18 17:28:11 -0700

    reading through tailwind docs. save for later

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,20 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
+    screens: {
+      'mob': '0px',
+      'tab': '481px',
+      'sm': '769px',
+      'lg': '1025px',
+      'xl': '1201px'
+    },
     extend: {},
   },
   plugins: [],
 }
 
+// light green: green 100
+// green: emerald 200
+// forest: cyan 800
+// light: zinc 100
+//lightest: slate 50

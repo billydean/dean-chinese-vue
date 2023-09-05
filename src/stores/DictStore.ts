8064be285205e02b3799e12ac88dfaa5d76ee9d7
Billy Dean <billy.goehring@gmail.com>
2023-09-05 15:13:17 -0700

    over to desktop

---
+++ src/stores/DictStore.ts
@@ -0,0 +1,13 @@
+import { defineStore } from 'pinia';
+import axios from 'axios';
+import { ref } from 'vue';
+
+export const useDictStore = defineStore('dict', () => {
+    const fetchByPinYin = async () => {
+        try {
+            const data = await axio 
+        }
+    }
+    
+    return { fetchByPinYin }
+})
--- src/stores/FlagStore.ts
+++ src/stores/FlagStore.ts
@@ -3,6 +3,7 @@ import { ref } from 'vue';
 
 export const useFlagsStore = defineStore('flags', () => {
     const openSearch = ref(false);
+    const openFilter = ref(false);
 
-    return { openSearch }
+    return { openSearch, openFilter }
 })

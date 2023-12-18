6340e57fcb9eda1323fdf85e2fa08cccec28bbc0
billydean <billy.goehring@gmail.com>
2023-12-18 15:36:19 -0800

    pretty messy, but getting three state toggle set up so I can collapse the search zone

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -9,6 +9,17 @@
 <template>
     
     <section class="search-zone">
+        <div class="switch-container">
+            <div class="switch">
+                <input type="radio" id="radio-pin" name="radio"/>
+                <label class="switch-labels" for="radio-pin">PRONUNCIATION</label>
+                <input type="radio" id="radio-char" name="radio"/>
+                <label class="switch-labels" for="radio-char">CHARACTER</label>
+                <input type="radio" id="radio-def" name="radio"/>
+                <label class="switch-labels" for="radio-def">DEFINITION</label>
+                <span id="switch-select"></span>
+            </div>
+        </div>
         <form @submit.prevent="">
             <div class="text-input">
                 <label for="pin-search">
@@ -48,10 +59,16 @@
         position: fixed;
         z-index: 30;
         bottom: calc(10px + 2rem);
-        width: 80%;
+        width: 50%;
         margin: 5px;
     }
 
+    .text-input input {
+        width: 70%;
+    }
+    .text-input button {
+        width: 15%;
+    }
     .text-input label::before {
         content: "Search for a character";
         font-size: 0;
@@ -59,4 +76,54 @@
         overflow: hidden;
     }
 
+    input[type="radio"], input[type="checkbox"] {
+        display: none;
+        cursor: pointer;
+    }
+
+    label {
+        cursor: pointer;
+    }
+    .switch {
+        position: relative;
+        height: 50px;
+        display: flex;
+        justify-content: space-around;
+        font-size: 0.8em;
+    }
+
+    .switch-container {
+        width: 75%;
+        /* margin: 0 auto; */
+    }
+    .switch-labels {
+        padding: 1rem;
+        z-index: 1;
+        width: 33.3%;
+        /* margin: auto; */
+    }
+
+    #switch-select {
+        position: absolute;
+        left: 0;
+        width: 34%;
+        height: 100%;
+        background: #14142a;
+        border: 1px solid #fff;
+        border-radius: 12px;
+        opacity: 0.5;
+        transition: 0.2s left ease;
+    }
+
+    #radio-pin:checked ~ #switch-select {
+        left: 0;
+    }
+
+    #radio-char:checked ~ #switch-select {
+        left: 33%;
+    }
+
+    #radio-def:checked ~ #switch-select {
+        left: 66%;
+    }
 </style>
